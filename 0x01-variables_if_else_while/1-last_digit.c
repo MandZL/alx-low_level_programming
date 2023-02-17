dc1d375c8548c58fc5305d2a3d181c980c137be5
“Mandisa <mandzlut@gmail.com>
2023-02-17 11:56:23 -0800

    last digt

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,7 +19,17 @@ int main(void)
 	/* your code goes there */
 	/* find last digit using n%10 and make new variable lastdigit*/
 	
-	if (n%10 > 5
-	
+	if ((n % 10) > 5)
+	{
+	       	printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
+	}
+	else if ((n % 10) == 0)
+	{
+		printf("Last digit of %d id %d and is 0\n", n, n % 10);
+	}
+	else if ((n %10) < 6 && (n % 10) != 0)
+	{
+		printf("Last digit of %d is %d and and is less than 6 and not 0\n", n, n %10);
+	}
 	return (0);
 }
