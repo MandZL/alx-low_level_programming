67ed8add9a426ac99fb13917d623ac75312c2c43
“Mandisa <mandzlut@gmail.com>
2023-02-17 12:52:45 -0800

    print nubers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - writes all single digit 
+ * numbers of base 10starting from 0
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	for (n = 0; n < 10; n++)
+	{
+		printf("%d\n", n);
+	}
+	return (0);
+}
