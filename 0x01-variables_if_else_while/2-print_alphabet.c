28b24e75bcc15441957c80f50d35106d47812d36
“Mandisa <mandzlut@gmail.com>
2023-02-17 12:21:40 -0800

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints lower case alphabet using putchar
+ * new line
+ * Return: Always 0 success
+ */
+
+int main(void)
+{
+	int x
+		for (x = 'A'; x <= 'Z', x++)
+		{
+			putchar(tolower(x));
+		}
+	putchar(\n);
+	return (0);
+}
+
