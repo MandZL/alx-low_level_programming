664f2e019629efc348e35e4a93b11819f5f017e2
“Mandisa <mandzlut@gmail.com>
2023-02-17 12:46:49 -0800

    alphabet exclude

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,13 +9,13 @@ int main(void)
 	char x = 'a';
 	while (x <= 'z')
 	{
-	if (x != 'e' && x != 'q')
-	{
-		putchar(x);
-	}
-	x++;
+		if (x != 'e' && x != 'q')
+		{
+			putchar(x);
+		}
+		x++;
 	}
 	putchar('\n');
-	return (0)
+	return (0);
 }
 
