dcfc1283048d5a2cc0d8781ff3d32ddf1a8f3564
“Mandisa <mandzlut@gmail.com>
2023-02-17 12:03:03 -0800

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,7 +5,8 @@
 /* more headers goes there */
 
 /**
- * main - input random number n and check if last digit is greater than 5, zero or less than 6 and not zero
+ * main - input random number n and check if last digit is
+ * greater than 5, zero or less than 6 and not zero
  * Return: Always 0 (success)
  */
 
@@ -25,11 +26,11 @@ int main(void)
 	}
 	else if ((n % 10) == 0)
 	{
-		printf("Last digit of %d id %d and is 0\n", n, n % 10);
+		printf("Last digit of %d is %d and is 0\n", n, n % 10);
 	}
 	else if ((n %10) < 6 && (n % 10) != 0)
 	{
-		printf("Last digit of %d is %d and and is less than 6 and not 0\n", n, n %10);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n %10);
 	}
 	return (0);
 }
