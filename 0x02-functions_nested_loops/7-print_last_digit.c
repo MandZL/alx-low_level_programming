81acf050b00b14cc6b1edc56a1f3d5abe9adfe5a
“Mandisa <mandzlut@gmail.com>
2023-02-21 11:24:05 -0800

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * int print_last_digit(int) - prints last digit
+ */
+int print_last_digit(int a)
+{
+	int last;
+	last = a%10;
+	if (a < 0)
+	{
+		last = last * (-1);
+	}
+}
