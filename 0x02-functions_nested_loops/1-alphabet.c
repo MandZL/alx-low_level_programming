d0f38ac1ce154eb23c99966afc036ecbb3073308
“Mandisa <mandzlut@gmail.com>
2023-02-21 10:06:15 -0800

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main -print alphabet in lower casex10
+ *
+ * Return 0 always success
+ */
+void print_alphabet(void);
+{
+	int n;
+	for (n = 'a', n <= 'z', n++)
+	{
+		_putchar(n);
+
+	}
+	_putchar('\n');
+}
