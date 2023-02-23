e4d87cbe1836827d6a9eaf067a5cd9ca56a035db
“Mandisa <mandzlut@gmail.com>
2023-02-23 11:04:45 -0800

    print 10 lines

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * more_numbers - print 10 lines of 1 to 14
+ */
+
+void more_numbers(void)
+{
+	int x;
+	int y = 0;
+	while (y <= 9)
+	{
+		for (x = 0; x <= 14; x++)
+		{
+			_putchar(x);
+			_putchar('\n');
+		}
+		y++;
+		_putchar('\n')
+	}
+}
+
