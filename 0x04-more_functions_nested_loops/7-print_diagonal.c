26e83c0cd62c072d22c5c5a4dcf06155055fc613
“Mandisa <mandzlut@gmail.com>
2023-02-23 12:05:37 -0800

    diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_diagonal - prints diagonal line
+ */
+
+void print_diagonal(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else 
+	{
+		int x;
+		int y;
+		for (x = 1; x <= n; x++)
+			for (y = 1; y <= n; y++)
+			{
+				if (x == y)
+				
+				{
+					_putchar('\');
+				}
+				else if (y < x)
+				{
+					_putchar(' ');
+				}
+			_putchar('\n');
+			}
+	}
+}
