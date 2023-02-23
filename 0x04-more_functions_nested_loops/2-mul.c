9d61dc1eaf16956cbbdc58c4ee70cca0c0116259
“Mandisa <mandzlut@gmail.com>
2023-02-23 09:51:31 -0800

    multiply

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * mul(int a, int b) - multiplies two integers
+ */
+int mul(int a, int b)
+{
+	int multiplication;
+	multiplication = a * b;
+	printf("%d", multiplication);
+}
