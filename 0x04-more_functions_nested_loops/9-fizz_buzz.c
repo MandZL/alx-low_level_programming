8199d5222923f512672aaa302496f0649026974b
“Mandisa <mandzlut@gmail.com>
2023-02-23 12:43:27 -0800

    fizzbuzzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,13 +12,17 @@ int main(void)
 		
 			printf("Fizz ");
 		}
-		else if (n % 5 == 0 && n % 3 != 0)
+		else if (n % 5 == 0 && n % 3 != 0 && n != 100)
 		{
 			printf("Buzz ");
 		}
 		else if (n % 3 == 0 && n % 5 == 0)
 		{
 			printf("FizzBuzz ");
+		}
+		else if (n = 100)
+		{
+			printf("Buzz");
 		}
 		else
 		{
