6f5435f6c9911127b62788ec381ef70d9a9c90b9
“Mandisa <mandzlut@gmail.com>
2023-02-23 10:14:16 -0800

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_numbers - prints numbers form 0 to 9
+ */
+void print_numbers(void)
+{
+	int x;
+	for (x < 10)
+	{
+		_putchar(x);
+	}
+	_putchar('\n');
+}
+
