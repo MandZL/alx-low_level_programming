0732d8d4caa7a2a0ad517ae7cf07e5c8a16e67b3
“Mandisa <mandzlut@gmail.com>
2023-02-23 11:35:40 -0800

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_line - function that prints straight line
+ */
+void print_line(int n)
+{
+	int x;
+	for (x = 0; x <= n; x++)
+	{
+		_putchar(_);
+	}
+	_putchar('\n');
+}
