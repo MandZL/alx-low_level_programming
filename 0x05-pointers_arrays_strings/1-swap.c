e00f76b70af98390e25246924eda4ff7c0915793
“Mandisa <mandzlut@gmail.com>
2023-02-27 08:15:38 -0800

    swap two integers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * swap_int - swaps the value of two intergers
+ * int n - int to swap
+ * int a, int b vlues to be swapped
+ */
+
+void swap_int(int *a, int *b)
+{
+	int n;
+	n = *a;
+	a* = b*;
+	*b = m;
+}
